331d25c250b81edc7b191ed3b33decedb29f48ba
Tom Coemans <tom.coemans@gmail.com>
2019-06-21 16:14:31 +0200

    DSI2-309 introductie mixin

--- vl-link.src.js
+++ vl-link.src.js
@@ -1,43 +1,47 @@
 import { NativeVlElement } from '/node_modules/vl-ui-core/vl-core.js';
 
+export const VlLinkElement = (SuperClass) => {
+    return class extends NativeVlElement(SuperClass) {
+        static get _observedAttributes() {
+            return [];
+        }
+    
+        static get _observedClassAttributes() {
+            return ['block'];
+        }
+    
+        connectedCallback() {
+            this.classList.add('vl-link');
+            this._setIconLinkAttribute();
+        }
+    
+        get _classPrefix() {
+            return 'vl-link--';
+        }
+    
+        get _stylePath() {
+            return '../style.css';
+        }
+    
+        get _iconElementen() {
+            return this.querySelectorAll('[is="vl-icon"]');
+        }
+    
+        _setIconLinkAttribute() {
+            this._iconElementen.forEach((icon) => {
+                icon.setAttribute('link', '');
+            });
+        }
+    }
+}
+
 /**
- * VlButton
+ * VlLink
  * @class
  * @classdesc Gebruik de vl-link om de gebruiker door te verwijzen naar een andere URL, bijvoorbeeld een nieuwe pagina of een document. <a href="demo/vl-link.html">Demo</a>.
  * 
  * @extends NativeVlElement
  */
-export class VlLink extends NativeVlElement(HTMLAnchorElement) {
-    static get _observedAttributes() {
-        return [];
-    }
-
-    static get _observedClassAttributes() {
-        return ['block'];
-    }
-
-    connectedCallback() {
-        this.classList.add('vl-link');
-        this._setIconLinkAttribute();
-    }
-
-    get _classPrefix() {
-        return 'vl-link--';
-    }
-
-    get _stylePath() {
-        return '../style.css';
-    }
-
-    get _iconElementen() {
-        return this.querySelectorAll('[is="vl-icon"]');
-    }
-
-    _setIconLinkAttribute() {
-        this._iconElementen.forEach((icon) => {
-            icon.setAttribute('link', '');
-        });
-    }
-}
+export class VlLink extends VlLinkElement(HTMLAnchorElement) {}
 
 customElements.define('vl-link', VlLink, {extends: 'a'});
