f1a201ec475d45b413e7dab3ecf9cc941f8db1bd
cambiph <incubusattax@gmail.com>
2019-09-05 13:12:22 +0200

    Update link naar demo

--- vl-link.src.js
+++ vl-link.src.js
@@ -44,7 +44,7 @@ export const VlLinkElement = (SuperClass) => {
  * 
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-link/releases/latest|Release notes}
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-link/issues|Issues}
- * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-ui-link.html|Demo}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-link.html|Demo}
  */
 export class VlLink extends VlLinkElement(HTMLAnchorElement) {}
 
