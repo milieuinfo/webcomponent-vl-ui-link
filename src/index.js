2905c351398ad65d9d20f21b8e45837fae523e17
Tom Coemans <tom.coemans@gmail.com>
2020-01-28 10:23:03 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = { 
-    VlLink: require('../vl-link.src.js')
+    VlLink: require('../vl-link.src.js').VlLink,
+    VlLinkElement: require('../vl-link.src.js').VlLinkElement,
+    VlButtonLink: require('../vl-link.src.js').VlButtonLink
 }
