9cc1c4431a5d64b6fcf9db6eb188dba781a1b886
Tom Coemans <tom.coemans@gmail.com>
2020-02-25 10:28:09 +0100

    package-lock

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+module.exports = { 
+    VlLink: require('../dist/vl-link.src.js').VlLink,
+    VlLinkElement: require('../dist/vl-link.src.js').VlLinkElement,
+    VlButtonLink: require('../dist/vl-link.src.js').VlButtonLink
+}
