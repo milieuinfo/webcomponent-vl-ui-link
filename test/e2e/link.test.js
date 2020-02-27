ae455d937dc18ea5e82a6a9f08c390a375d5a541
Sander Kleykens <sander.kleykens@avnu.be>
2020-02-27 16:20:11 +0100

    Introduceer e2e testen

--- test/e2e/link.test.js
+++ test/e2e/link.test.js
@@ -8,6 +8,19 @@ describe('vl-link', async () => {
         return vlLinkPage.load();
     });
 
-    it("Dummy test om de browsers te laten sluiten", () => {
+    it('Als gebruiker kan ik een icoon bij de link zien', async () => {
+        const link = await vlLinkPage.getLinkWithIcon();
+
+        const icons = await link.getIcons();
+        await assert.lengthOf(icons, 1);
+        await assert.eventually.equal(icons[0].getType(), 'arrow-right-fat');
+    });
+
+    it('Als gebruiker kan ik een button gestyled als link zien, inclusief icoon', async () => {
+        const buttonLink = await vlLinkPage.getButtonLink();
+
+        const icons = await buttonLink.getIcons();
+        await assert.lengthOf(icons, 1);
+        await assert.eventually.equal(icons[0].getType(), 'arrow-right-fat');
     });
 });
