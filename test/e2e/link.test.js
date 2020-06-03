ca6090d0104d4db0cab33c477b97567ed10b2de7
coemans <tom.coemans@gmail.com>
2020-06-03 14:14:47 +0200

    Introductie vl-ui-demo en ESlint (#77)

--- test/e2e/link.test.js
+++ test/e2e/link.test.js
@@ -1,22 +1,22 @@
-const { assert, driver } = require('vl-ui-core').Test.Setup;
+const {assert, driver} = require('vl-ui-core').Test.Setup;
 const VlLinkPage = require('./pages/vl-link.page');
 
 describe('vl-link', async () => {
-    const vlLinkPage = new VlLinkPage(driver);
+  const vlLinkPage = new VlLinkPage(driver);
 
-    before(() => {
-        return vlLinkPage.load();
-    });
+  before(() => {
+    return vlLinkPage.load();
+  });
 
-    it('Als gebruiker kan ik de tekst van een link zien', async () => {
-        const link = await vlLinkPage.getLink();
+  it('Als gebruiker kan ik de tekst van een link zien', async () => {
+    const link = await vlLinkPage.getLink();
 
-        await assert.eventually.equal(link.getText(), 'Terug naar overzicht');
-    });
+    await assert.eventually.equal(link.getText(), 'Terug naar overzicht');
+  });
 
-    it('Als gebruiker kan ik de tekst van een button gestyled als link zien', async () => {
-        const buttonLink = await vlLinkPage.getButtonLink();
+  it('Als gebruiker kan ik de tekst van een button gestyled als link zien', async () => {
+    const buttonLink = await vlLinkPage.getButtonLink();
 
-        await assert.eventually.equal(buttonLink.getText(), 'Demo link');
-    });
+    await assert.eventually.equal(buttonLink.getText(), 'Demo link');
+  });
 });
