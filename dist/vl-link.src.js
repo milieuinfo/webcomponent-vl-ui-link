60025f84be3755f4ce9c7dad79a71f6d30e39859
Tom Coemans <tom.coemans@gmail.com>
2020-06-04 14:48:59 +0200

    vl-ui-core update trim e2e text

--- dist/vl-link.src.js
+++ dist/vl-link.src.js
@@ -1,61 +1,65 @@
-import { NativeVlElement, define } from 'vl-ui-core';
+import {nativeVlElement, define} from vl-ui-core;
 
-export const VlLinkElement = (SuperClass) => {
-    return class extends NativeVlElement(SuperClass) {
-        static get _observedAttributes() {
-            return [];
-        }
-    
-        static get _observedClassAttributes() {
-            return ['block'];
-        }
-    
-        connectedCallback() {
-            this.classList.add('vl-link');
-            this._setIconLinkAttribute();
-        }
-    
-        get _classPrefix() {
-            return 'vl-link--';
-        }
-    
-        get _iconElementen() {
-            return this.querySelectorAll('[is="vl-icon"]');
-        }
-    
-        _setIconLinkAttribute() {
-            this._iconElementen.forEach((icon) => {
-                icon.setAttribute('link', '');
-            });
-        }
+export const vlLinkElement = (SuperClass) => {
+  return class extends nativeVlElement(SuperClass) {
+    static get _observedAttributes() {
+      return [];
     }
+
+    static get _observedClassAttributes() {
+      return ['block'];
+    }
+
+    connectedCallback() {
+      this.classList.add('vl-link');
+      this._setIconLinkAttribute();
+    }
+
+    get _classPrefix() {
+      return 'vl-link--';
+    }
+
+    get _iconElementen() {
+      return this.querySelectorAll('[is="vl-icon"]');
+    }
+
+    _setIconLinkAttribute() {
+      this._iconElementen.forEach((icon) => {
+        icon.setAttribute('link', '');
+      });
+    }
+  };
 };
+
+export const VlLinkElement = vlLinkElement;
 
 /**
  * VlLink
  * @class
  * @classdesc Gebruik de vl-link om de gebruiker door te verwijzen naar een andere URL, bijvoorbeeld een nieuwe pagina of een document.
- * 
- * @extends NativeVlElement
- * 
+ *
+ * @extends HTMLAnchorElement
+ * @mixin vlLinkElement
+ *
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-link/releases/latest|Release notes}
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-link/issues|Issues}
  * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-link.html|Demo}
  */
-export class VlLink extends VlLinkElement(HTMLAnchorElement) {}
+export class VlLink extends vlLinkElement(HTMLAnchorElement) {}
 
 /**
  * VlButtonLink
  * @class
  * @classdesc Een button gestyled als link.
  *
- * @extends NativeVlElement
+ * @extends HTMLButtonElement
+ * @mixin vlLinkElement
  *
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-link/releases/latest|Release notes}
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-link/issues|Issues}
  * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-link.html|Demo}
  */
-export class VlButtonLink extends VlLinkElement(HTMLButtonElement) {}
+export class VlButtonLink extends vlLinkElement(HTMLButtonElement) {}
 
 define('vl-link', VlLink, {extends: 'a'});
 define('vl-button-link', VlButtonLink, {extends: 'button'});
