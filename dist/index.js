ee4763fb11d815742d812265b58414019fb038c2
Tom Coemans <tom.coemans@gmail.com>
2020-08-14 11:19:45 +0200

    dist cache

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  VlLink: require('../dist/vl-link.src.js').VlLink,
+  VlLinkElement: require('../dist/vl-link.src.js').VlLinkElement,
+  VlButtonLink: require('../dist/vl-link.src.js').VlButtonLink,
+}
+;
---
+++ dist/vl-link.src.js
@@ -0,0 +1,77 @@
+import {nativeVlElement, define} from 'vl-ui-core';
+
+/**
+ * Gebruik de link mixin in combinatie met link elementen.
+ * @mixin vlLinkElement
+ *
+ * @param {Object} SuperClass - Class die als base class gebruikt zal worden.
+ * @return {Object} class
+ */
+export const vlLinkElement = (SuperClass) => {
+  return class extends nativeVlElement(SuperClass) {
+    static get _observedAttributes() {
+      return [];
+    }
+
+    static get _observedClassAttributes() {
+      return ['block'];
+    }
+
+    connectedCallback() {
+      this.classList.add('vl-link');
+      this._setIconLinkAttribute();
+    }
+
+    get _classPrefix() {
+      return 'vl-link--';
+    }
+
+    get _iconElementen() {
+      return this.querySelectorAll('[is="vl-icon"]');
+    }
+
+    _setIconLinkAttribute() {
+      this._iconElementen.forEach((icon) => {
+        icon.setAttribute('link', '');
+      });
+    }
+  };
+};
+
+export const VlLinkElement = vlLinkElement;
+
+/**
+ * VlLink
+ * @class
+ * @classdesc Gebruik de vl-link om de gebruiker door te verwijzen naar een andere URL, bijvoorbeeld een nieuwe pagina of een document.
+ *
+ * @extends HTMLAnchorElement
+ * @mixes vlLinkElement
+ *
+ *@property {string} data-vl-block - Attribuut zorgt ervoor dat het element als block getoond wordt.
+ *
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-link/releases/latest|Release notes}
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-link/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-link.html|Demo}
+ */
+export class VlLink extends vlLinkElement(HTMLAnchorElement) {}
+
+/**
+ * VlButtonLink
+ * @class
+ * @classdesc Een button gestyled als link.
+ *
+ * @extends HTMLButtonElement
+ * @mixes vlLinkElement
+ *
+ * @property {string} data-vl-block - Attribuut zorgt ervoor dat het element als block getoond wordt.
+ *
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-link/releases/latest|Release notes}
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-link/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-link.html|Demo}
+ */
+export class VlButtonLink extends vlLinkElement(HTMLButtonElement) {}
+
+define('vl-link', VlLink, {extends: 'a'});
+define('vl-button-link', VlButtonLink, {extends: 'button'});
+
