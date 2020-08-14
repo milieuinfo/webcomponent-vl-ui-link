b6c555d1892e7e1b4755d70e9862e55501901776
Tom Coemans <tom.coemans@gmail.com>
2020-08-14 11:19:33 +0200

    dist cache

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  VlLink: require('../dist/vl-link.src.js').VlLink,
-  VlLinkElement: require('../dist/vl-link.src.js').VlLinkElement,
-  VlButtonLink: require('../dist/vl-link.src.js').VlButtonLink,
-}
-;
