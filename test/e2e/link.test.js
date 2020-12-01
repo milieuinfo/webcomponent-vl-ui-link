acbfd69a49d831a014e63f939a15e5a841b83786
Tom Coemans <tom.coemans@gmail.com>
2020-12-01 14:47:34 +0100

    browserstack

--- test/e2e/link.test.js
+++ test/e2e/link.test.js
@@ -1,19 +1,20 @@
-const {assert, driver} = require('vl-ui-core').Test.Setup;
+const {assert, getDriver} = require('vl-ui-core').Test.Setup;
 const VlLinkPage = require('./pages/vl-link.page');
 
 describe('vl-link', async () => {
-  const vlLinkPage = new VlLinkPage(driver);
+  let vlLinkPage;
 
   before(() => {
+    vlLinkPage = new VlLinkPage(getDriver());
     return vlLinkPage.load();
   });
 
-  it('Als gebruiker kan ik de tekst van een link zien', async () => {
+  it('als gebruiker kan ik de tekst van een link zien', async () => {
     const link = await vlLinkPage.getLink();
     await assert.eventually.equal(link.getText(), 'Terug naar overzicht');
   });
 
-  it('Als gebruiker kan ik de tekst van een button gestyled als link zien', async () => {
+  it('als gebruiker kan ik de tekst van een button gestyled als link zien', async () => {
     const buttonLink = await vlLinkPage.getButtonLink();
     await assert.eventually.equal(buttonLink.getText(), 'Demo link');
   });
